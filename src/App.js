b8dc68ae324cb98a436416c1635ea99f623063c5
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-06 20:32:39 +0000

    paginas

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
-//Linha que importa o histórico
 import { useState } from "react";
-import history from "./assets/data/history.json"
 import { numeroDePlays, musicasDiferentes, minutosTotaisOuvidos, mediaTempoDiario, horaMaisOuvida, estacaoMaisOuvida, topCemArtistas, topCemMusicas, numPlaysArtista, musicasDiferentesArtista, minutosOuvidosArtista, playsDoArtista, topVinteMusicasPorArtista, posicaoTopCemArtista, artistaEstacaoMaisOuvida } from "./common/index.js";
 
 function App() {
-  const [paginaAtual, setPaginaAtual] = useState({
-    path: "index",
-    context: ""
-  })
+  const [paginaAtual, setPaginaAtual] = useState("index");
+  const onChangePage = (page) => { setPaginaAtual(page); };
 
-
-  const paginas = new Map([
-    ["index", <Pagina onChangePage={(page) => setPaginaAtual({path: page, context: ""})} />],
-    ["cenas", <Pagina2 onChangePage={(page) => setPaginaAtual({path: page, context: ""})} />],
-    ["coisas", <div>coisas</div>],
-    // ["top-musica", <TopPage context="artista"/>],
-    // ["top-artista", <TopPage context="musica"/>],
-    // ["artista", <ArtistPage artist="Kendrick Lamar"/>],
-    // ["top-20", <ArtistTopPage artist="Kendrick Lamar"/>],
-
-  ])
-
-  //Como podes ver na consola, é um array de objetos
+  const paginas = {
+    "index": <HomePage onChangePage={onChangePage} />,
+    "top-artista": <TopArtistPage onChangePage={onChangePage} />,
+    "top-musica": <TopMusicPage onChangePage={onChangePage} />,
+    "artista": <ArtistPage artist="Kendrick Lamar" onChangePage={onChangePage} />,
+    "top-20": <TopArtistMusicPage artist="Kendrick Lamar" onChangePage={onChangePage} />,
+  };
 
   return (
     <div className="App">
-      {
-        paginas.has(paginaAtual.path) ? paginas.get(paginaAtual.path) : <div>ERROR </div>
-      }
-      {/* <p>top 100 artistas: {topCemArtistas("sempre")}</p>
-      <p>top 100 musicas: {topCemMusicas("4semanas")}</p>
-      <p>numero de plays do artista: {numPlaysArtista("TOOL")}</p>
-      <p>numero de musicas diferentes por artista: {musicasDiferentesArtista("TOOL")}</p>
-      <p>minutos ouvidos por artista: {minutosOuvidosArtista("TOOL")}</p>
-      <p>percentagem de plays que são do artista: {playsDoArtista("TOOL")}%</p>
-      <p>top 20 musicas por artista em ms: {topVinteMusicasPorArtista("4semanas", "TOOL")}</p>
-      <p>index do artista no top 100: {posicaoTopCemArtista("Kendrick Lamar")}</p>
-      <p>em que estação o artista é mais ouvido: {artistaEstacaoMaisOuvida("TOOL")}</p> */}
+      {paginas[paginaAtual] || <div>ERROR: PAGE NOT FOUND</div>}
     </div>
-    
   );
 }
 
-
-
-function Pagina ({onChangePage}) {
+function HomePage({onChangePage}) {
   return (
     <div className="page" >
       <p>numero de plays no total: {numeroDePlays()} </p>
@@ -53,18 +29,49 @@ function Pagina ({onChangePage}) {
       <p>media de tempo diario: {mediaTempoDiario()} min por dia</p>
       <p>hora mais ouvida: {horaMaisOuvida()}</p>
       <p>estação mais ouvida: {estacaoMaisOuvida()}</p>
-      
-      <button onClick={() => onChangePage("cenas")}>Página 2</button>
+      <button onClick={() => onChangePage("top-artista")}>Top Page Artistas</button>
     </div>
   )
 }
 
-function Pagina2 ({onChangePage}) {
+
+function TopArtistPage({onChangePage}) {
   return (
     <div className="page" >
-    <p>aaaa</p>
-      
-      <button onClick={() => onChangePage("coisas")}>Página 3</button>
+      <p>top 100 artistas: {topCemArtistas("sempre")}</p>
+      <button onClick={() => onChangePage("top-musica")}>Top Page Músicas</button>
+    </div>
+  )
+}
+
+function TopMusicPage({onChangePage}) {
+  return (
+    <div className="page" >
+      <p>top 100 musicas: {topCemMusicas("sempre")}</p>
+      <button onClick={() => onChangePage("artista")}>Artista Page</button>
+    </div>
+  )
+}
+
+function ArtistPage({onChangePage}) {
+  return (
+    <div className="page" >
+      <p>numero de plays do artista: {numPlaysArtista("TOOL")}</p>
+      <p>numero de musicas diferentes por artista: {musicasDiferentesArtista("TOOL")}</p>
+      <p>minutos ouvidos por artista: {minutosOuvidosArtista("TOOL")}</p>
+      <p>percentagem de plays que são do artista: {playsDoArtista("TOOL")}%</p>
+      <p>index do artista no top 100: {posicaoTopCemArtista("Kendrick Lamar")}</p>
+      <p>em que estação o artista é mais ouvido: {artistaEstacaoMaisOuvida("TOOL")}</p>
+      <button onClick={() => onChangePage("top-20")}>Top 20 Artista</button>
+    </div>
+  )
+}
+
+function TopArtistMusicPage({onChangePage}) {
+  return (
+    <div className="page" >
+      <p>top 20 musicas por artista em ms: {topVinteMusicasPorArtista("4semanas", "TOOL")}</p>
+      <button onClick={() => onChangePage("index")}>Voltar á Casa</button>
     </div>
   )
 }
