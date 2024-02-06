e4f5583c75f852764c2804f841ee9a86ae1f3faf
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-06 17:42:59 +0000

    paginas

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   const paginas = new Map([
     ["index", <Pagina onChangePage={(page) => setPaginaAtual({path: page, context: ""})} />],
-    ["cenas", <div>cenas</div>],
+    ["cenas", <Pagina2 onChangePage={(page) => setPaginaAtual({path: page, context: ""})} />],
     ["coisas", <div>coisas</div>],
     // ["top-musica", <TopPage context="artista"/>],
     // ["top-artista", <TopPage context="musica"/>],
@@ -28,13 +28,7 @@ function App() {
       {
         paginas.has(paginaAtual.path) ? paginas.get(paginaAtual.path) : <div>ERROR </div>
       }
-      {/* <p>numero de plays no total: {numeroDePlays()} </p>
-      <p>numero de musicas diferentes ja ouvidas: {musicasDiferentes()} </p>
-      <p>numero de minutos ouvidos: {minutosTotaisOuvidos()} min </p>
-      <p>media de tempo diario: {mediaTempoDiario()} min por dia</p>
-      <p>hora mais ouvida: {horaMaisOuvida()}</p>
-      <p>estação mais ouvida: {estacaoMaisOuvida()}</p>
-      <p>top 100 artistas: {topCemArtistas("sempre")}</p>
+      {/* <p>top 100 artistas: {topCemArtistas("sempre")}</p>
       <p>top 100 musicas: {topCemMusicas("4semanas")}</p>
       <p>numero de plays do artista: {numPlaysArtista("TOOL")}</p>
       <p>numero de musicas diferentes por artista: {musicasDiferentesArtista("TOOL")}</p>
@@ -44,6 +38,7 @@ function App() {
       <p>index do artista no top 100: {posicaoTopCemArtista("Kendrick Lamar")}</p>
       <p>em que estação o artista é mais ouvido: {artistaEstacaoMaisOuvida("TOOL")}</p> */}
     </div>
+    
   );
 }
 
@@ -51,9 +46,25 @@ function App() {
 
 function Pagina ({onChangePage}) {
   return (
-    <div >
-      Index
-      <button onClick={() => onChangePage("cenas")}>Artista coias</button>
+    <div className="page" >
+      <p>numero de plays no total: {numeroDePlays()} </p>
+      <p>numero de musicas diferentes ja ouvidas: {musicasDiferentes()} </p>
+      <p>numero de minutos ouvidos: {minutosTotaisOuvidos()} min </p>
+      <p>media de tempo diario: {mediaTempoDiario()} min por dia</p>
+      <p>hora mais ouvida: {horaMaisOuvida()}</p>
+      <p>estação mais ouvida: {estacaoMaisOuvida()}</p>
+      
+      <button onClick={() => onChangePage("cenas")}>Página 2</button>
+    </div>
+  )
+}
+
+function Pagina2 ({onChangePage}) {
+  return (
+    <div className="page" >
+    <p>aaaa</p>
+      
+      <button onClick={() => onChangePage("coisas")}>Página 3</button>
     </div>
   )
 }
