b280504435beeb6140462d06e7438af0625994fb
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-06 11:12:12 +0000

    mediaTempoDiario, horaMaisOuvida

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //Linha que importa o histórico
 import history from "./assets/data/history.json"
-import { numeroDePlays, musicasDiferentes, minutosTotaisOuvidos } from "./common/index.js";
+import { numeroDePlays, musicasDiferentes, minutosTotaisOuvidos, mediaTempoDiario, horaMaisOuvida } from "./common/index.js";
 
 function App() {
 
@@ -12,6 +12,8 @@ function App() {
       <p>numero de plays no total: {numeroDePlays()} </p>
       <p>numero de musicas diferentes ja ouvidas: {musicasDiferentes()} </p>
       <p>numero de minutos ouvidos: {minutosTotaisOuvidos()} min </p>
+      <p>media de tempo diario: {mediaTempoDiario()} min por dia</p>
+      <p>hora mais ouvida: entre as {horaMaisOuvida()}h e {horaMaisOuvida()+1}h</p>
     </div>
   );
 }
--- src/common/index.js
+++ src/common/index.js
@@ -22,5 +22,32 @@ export function minutosTotaisOuvidos(){
 }
 
 export function mediaTempoDiario(){
-  
+  const primeiroDia = new Date(history[0].ts);
+  const ultimoDia = new Date(history[history.length-1].ts);
+  const diasOuvidos = (ultimoDia.getTime() - primeiroDia.getTime()) / (1000 * 3600 * 24);
+  return Math.round(minutosTotaisOuvidos() / diasOuvidos);
+}
+
+
+export function horaMaisOuvida() {
+    const horas = history.map(e => {
+        const tempo = new Date(e.ts)
+        return tempo.getHours();
+      });
+
+    const contagemHoras = {};
+    horas.forEach(hora => {
+        contagemHoras[hora] = (contagemHoras[hora] || 0) + 1;
+    });
+
+    let modaHora;
+    let contagemModa = 0;
+    for (const hora in contagemHoras) {
+        if (contagemHoras[hora] > contagemModa) {
+            modaHora = hora;
+            contagemModa = contagemHoras[hora];
+        }
+    }
+
+    return Number(modaHora);
 }
