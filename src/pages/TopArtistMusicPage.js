65f3bc7828075fcf917d6e7e566e64fc4ae46f6c
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-07 10:41:02 +0000

    Botões dos periodos

--- src/pages/TopArtistMusicPage.js
+++ src/pages/TopArtistMusicPage.js
@@ -10,14 +10,6 @@ export function TopArtistMusicPage({onChangePage}) {
 
     return (
       <div className="page">
-        <p className="mb-2 text-lg">Escolha o período:</p>
-        <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
-            <button onClick={() => onChangePeriodo("sempre")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "sempre" ? "bg-blue-600" : ""}`}>Sempre</button>
-            <button onClick={() => onChangePeriodo("4semanas")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "4semanas" ? "bg-blue-600" : ""}`}>Últimas 4 semanas</button>
-            <button onClick={() => onChangePeriodo("6meses")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "6meses" ? "bg-blue-600" : ""}`}>Últimos 6 meses</button>
-            <button onClick={() => onChangePeriodo("últimoAno")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "últimoAno" ? "bg-blue-600" : ""}`}>Último ano</button>
-        </div>
-
         <p className="mb-2 text-lg">Insira o nome do artista:</p>
         <input list="artistas" type="text" value={artista} onChange={handleInputChange} className="mb-4 p-2 border border-gray-300 rounded-md" />
         <datalist id="artistas">
@@ -28,6 +20,15 @@ export function TopArtistMusicPage({onChangePage}) {
         
         <p className="mb-2 text-lg">Top 20 músicas por artista em ms:</p>
         <p>{topVinteMusicasPorArtista(periodo, artista)}</p>
+
+        <div style={{ paddingTop: "1rem" }}>
+            <div className="grid grid-cols-4 gap-2 fixed bottom-0 left-0 w-full mb-16">
+                <button onClick={() => onChangePeriodo("4semanas")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "4semanas" ? "bg-blue-600" : ""}`}>4 semanas</button>
+                <button onClick={() => onChangePeriodo("6meses")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "6meses" ? "bg-blue-600" : ""}`}>6 meses</button>
+                <button onClick={() => onChangePeriodo("últimoAno")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "últimoAno" ? "bg-blue-600" : ""}`}>Último ano</button>
+                <button onClick={() => onChangePeriodo("sempre")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "sempre" ? "bg-blue-600" : ""}`}>Sempre</button>   
+            </div>
+        </div>
 
         <Menu onChangePage={onChangePage} />
         <div className="mb-16"></div>
