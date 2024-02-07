a18cefeebcf5a179ab8eacfa231b998ae3fc5133
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-07 10:03:30 +0000

    One top page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -5,8 +5,7 @@ export function Menu({ onChangePage }) {
     <nav className="fixed bottom-0 left-0 w-full bg-gray-800 p-4">
       <ul className="flex justify-between items-center">
         <li><button className="text-white hover:underline" onClick={() => onChangePage("index")}>Home</button></li>
-        <li><button className="text-white hover:underline" onClick={() => onChangePage("top-artista")}>Top Artistas</button></li>
-        <li><button className="text-white hover:underline" onClick={() => onChangePage("top-musica")}>Top Músicas</button></li>
+        <li><button className="text-white hover:underline" onClick={() => onChangePage("top-musica")}>Top</button></li>
         <li><button className="text-white hover:underline" onClick={() => onChangePage("artista")}>Artista</button></li>
         <li><button className="text-white hover:underline" onClick={() => onChangePage("top-20")}>Top 20</button></li>
       </ul>
--- src/pages/TopMusicPage.js
+++ src/pages/TopMusicPage.js
@@ -2,25 +2,30 @@ import React, { useState } from "react";
 import { topCemMusicas } from "../common/index.js";
 import { Menu } from "./Menu";
 
-export function TopMusicPage({ onChangePage }) {
+export function TopMusicPage({ onChangePage, currentPage }) {
   const [periodo, setPeriodo] = useState("sempre");
   const onChangePeriodo = (novoPeriodo) => { setPeriodo(novoPeriodo); };
 
   return (
     <div className="page">
-      <p className="mb-2 text-lg">Escolha o período:</p>
-      <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
-        <button onClick={() => onChangePeriodo("sempre")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "sempre" ? "bg-blue-600" : ""}`}>Sempre</button>
-        <button onClick={() => onChangePeriodo("4semanas")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "4semanas" ? "bg-blue-600" : ""}`}>Últimas 4 semanas</button>
-        <button onClick={() => onChangePeriodo("6meses")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "6meses" ? "bg-blue-600" : ""}`}>Últimos 6 meses</button>
-        <button onClick={() => onChangePeriodo("últimoAno")} className={`w-full px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "últimoAno" ? "bg-blue-600" : ""}`}>Último ano</button>
-      </div>
+      <div className="flex">
+        <button onClick={() => onChangePage("top-musica")} className={`flex-1 px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${ currentPage === "top-musica" ? "bg-blue-600" : "" }`} > Top Page Músicas </button>
+        <button onClick={() => onChangePage("top-artista")} className={`flex-1 px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${ currentPage === "top-artista" ? "bg-blue-600" : "" }`}> Top Page Artistas </button>
+     </div>
 
       <p className="mb-2 text-lg">Top 100 músicas:</p>
       <p>{topCemMusicas(periodo)}</p>
+
+      <div className="grid grid-cols-4 gap-2 fixed bottom-0 left-0 w-full mb-16">
+        <button onClick={() => onChangePeriodo("sempre")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "sempre" ? "bg-blue-600" : ""}`}>Sempre</button>
+        <button onClick={() => onChangePeriodo("4semanas")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "4semanas" ? "bg-blue-600" : ""}`}>Últimas 4 semanas</button>
+        <button onClick={() => onChangePeriodo("6meses")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "6meses" ? "bg-blue-600" : ""}`}>Últimos 6 meses</button>
+        <button onClick={() => onChangePeriodo("últimoAno")} className={`px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 ${periodo === "últimoAno" ? "bg-blue-600" : ""}`}>Último ano</button>
+      </div>
 
       <Menu onChangePage={onChangePage} />
       <div className="mb-16"></div>
     </div>
   )
 }
+
