273cc5fe02070a88d0641f9d1a8172bf83c6b092
Vanessa <vanessa.gomes.m.silva@gmail.com>
2024-02-08 11:26:41 +0000

    mais uma cor

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,32 +5,36 @@ import ArtistPhoto from "../components/artist_photo.js";
 import MusicAlbumArt from "../components/album.js";
 import LogoWithText from "../components/toplogo.js";
 
-export function HomePage({onChangePage}) {
-    return (
-      
-      <div className="page bg-lightgrey" >
-        <LogoWithText />
+export function HomePage({ onChangePage }) {
+  return (
 
+    <div className="page bg-lightgrey flex flex-col items-center" >
+
+      <div className="w-full">
+        <LogoWithText />
+      </div>
+
+      <div className="mt-20 mx-7">
         <h1 className="font-PressStart2p text-xl w-44">Wilson Felizardo</h1>
         <p className="font-JetbrainsMono text-xs text-green">Estoy usando o spotiDados. Dale!</p>
-        
-        <button className="border-black border-2 px-5 py-4 font-PressStart2p text-xxs">Editar Perfil</button>
-        <button className="border-black border-2 px-5 py-4 font-PressStart2p text-xxs">Share Profile</button>
-    
-        <p className="mb-2 text-lg">Número de plays no total: { numeroDePlays() }.</p>
-        <p className="mb-2 text-lg">Número de músicas diferentes já ouvidas: { musicasDiferentes() }.</p>
-        <p className="mb-2 text-lg">Número de minutos ouvidos: { minutosTotaisOuvidos() } min. </p>
-        <p className="mb-2 text-lg">Média de tempo diário: { mediaTempoDiario() } min por dia.</p>
-        <p className="mb-2 text-lg">Hora mais ouvida: { horaMaisOuvida() }.</p>
-        <p className="mb-2 text-lg">Estação do ano que mais ouve música: { estacaoMaisOuvida() }.</p>
+        <div className="flex flex">
+          <button className="border-black border-2 px-5 py-4 font-PressStart2p text-xxs">Editar Perfil</button>
+          <button className="border-black border-2 px-5 py-4 font-PressStart2p text-xxs">Share Profile</button>
+        </div>
+      </div>
+
+      <p className="mb-2 text-lg">Número de plays no total: {numeroDePlays()}.</p>
+      <p className="mb-2 text-lg">Número de músicas diferentes já ouvidas: {musicasDiferentes()}.</p>
+      <p className="mb-2 text-lg">Número de minutos ouvidos: {minutosTotaisOuvidos()} min. </p>
+      <p className="mb-2 text-lg">Média de tempo diário: {mediaTempoDiario()} min por dia.</p>
+      <p className="mb-2 text-lg">Hora mais ouvida: {horaMaisOuvida()}.</p>
+      <p className="mb-2 text-lg">Estação do ano que mais ouve música: {estacaoMaisOuvida()}.</p>
 
 
-      <MusicAlbumArt albumName="Jesus is King" />
-      
 
       <Menu onChangePage={onChangePage} />
       <div className="mb-16"></div>
-    </div>
+    </div >
   )
 }
 
