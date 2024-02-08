bc4a6404d9210a90a69e6a5e4b02377564e7f49b
RM <devRMoreira@gmail.com>
2024-02-08 09:32:47 +0000

    Fonts importadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    fontFamily: {
+      "PressStart2p": ["'Press Start 2P'", "sans-serif"],
+      "JetbrainsMono": ["JetBrains Mono", "sans-serif"]
+    }
   },
   plugins: [],
 }
