ee631532445f46ae0e03bf32fb59e622ba754a1d
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-08 09:06:52 +0000

    icons no menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { ReactComponent as HomeIcon } from "../assets/share.svg";
+import { ReactComponent as TopIcon } from "../assets/settings.svg";
+import { ReactComponent as SearchIcon } from "../assets/search.svg";
+
 
 export function Menu({ onChangePage }) {
   return (
     <nav className="fixed bottom-0 left-0 w-full bg-gray-800 p-4">
       <ul className="flex justify-between items-center">
-        <li><button className="text-white hover:underline" onClick={() => onChangePage("index")}>Home</button></li>
-        <li><button className="text-white hover:underline" onClick={() => onChangePage("top-musica")}>Top</button></li>
-        <li><button className="text-white hover:underline" onClick={() => onChangePage("artista")}>Artista</button></li>
+        <li>
+          <button className="text-white hover:underline" onClick={() => onChangePage("index")}>
+            <HomeIcon width="24" height="24" />
+          </button>
+        </li>
+        <li>
+          <button className="text-white hover:underline" onClick={() => onChangePage("top-musica")}>
+            <TopIcon width="24" height="24" />
+          </button>
+        </li>
+        <li>
+          <button className="text-white hover:underline" onClick={() => onChangePage("artista")}>
+            <SearchIcon width="24" height="24" />
+          </button>
+        </li>
       </ul>
     </nav>
   );
