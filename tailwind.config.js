e8d95910e1b0ac74c9e1ac1f2d222b13b24295a3
Vanessa <vanessa.gomes.m.silva@gmail.com>
2024-02-08 09:23:24 +0000

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      'lightgrey': '#ffffff',
+      'blue': '#0228F0',
+      'lighterblue': '#6277E4',
+      'green': '#565584',
+    },
   },
   plugins: [],
 }
