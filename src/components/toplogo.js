9963c5cb1726db85e2d6d4d48a90544c696bcf8f
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-02-08 10:18:50 +0000

    topdd

--- src/components/toplogo.js
+++ src/components/toplogo.js
@@ -5,7 +5,7 @@ const LogoWithText = () => {
   return (
     <div className="flex items-center ml-3 mt-3 mb-2">
       <img src= {logo} alt="Logo" className="w-12 h-12 mr-2" />
-      <span className="text-xl font-semibold ">Spotidados</span>
+      <span className="text-xs font-PressStart2p font-semibold ">Spotidados</span>
     </div>
   );
 };
