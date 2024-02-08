b820e024677bb9c8ea2503164aae019460c8620c
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-08 14:23:32 +0000

    cards artist page

--- src/components/toplogo.js
+++ src/components/toplogo.js
@@ -3,7 +3,7 @@ import logo from '../assets/logo.svg'
 
 const LogoWithText = () => {
   return (
-    <div className="flex items-center pl-3 pt-3 pb-2 fixed bg-lightgrey w-full">
+    <div className="flex items-center pl-3 pt-3 pb-2 fixed bg-lightgrey w-full top-0">
       <img src= {logo} alt="Logo" className="w-12 h-12 mr-2" />
       <span className="text-xs font-PressStart2p font-semibold ">Spotidados</span>
     </div>
