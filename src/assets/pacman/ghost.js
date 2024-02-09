fb61c11fe053af2b48ef0cfd057f75068ded3a48
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-02-09 16:31:36 +0000

    pcp

---
+++ src/assets/pacman/ghost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ghost.css';
+import ghost from './ghost.png'
+
+function Ghost() {
+  return (
+
+    <img id="ghost" src={ghost} alt="Ghost"></img>
+
+  );
+}
+
+export default Ghost;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,6 +4,7 @@ import LogoWithText from "../components/toplogo.js";
 import { ArtistPage } from "./ArtistPage.js";
 import { artistNamesArr } from "../common/index";
 import Pacman2 from "../assets/pacman/PacmanAnimated.js";
+import Ghost from "../assets/pacman/ghost.js";
 
 export function SearchPage({ onChangePage }) {
   const [artista, setArtista] = useState("");
@@ -18,7 +19,7 @@ export function SearchPage({ onChangePage }) {
           </div>
 
           <div className="flex flex-grow justify-center items-center shadow-sm relative">
-            <div className="absolute top-12 mt-12"><Pacman2 /></div>
+            <div className="absolute top-14 mt-12"><Pacman2 /></div>
             <div className="text-center border-2 border-black w-80 h-60 flex flex-col justify-center items-center p-4">
               <p className="mb-2 text-sm font-PressStart2p">Procura um artista</p>
               <input list="artistas" type="text" value={artista} className="font-JetbrainsMono font-extrabold text-lg text-black border-black border-2 bg-greybar mb-4 p-2 rounded-none w-full" onChange={handleInputChange} />
@@ -28,8 +29,10 @@ export function SearchPage({ onChangePage }) {
                 <option key={artista} value={artista} className="font-PressStart2p text-black" />
               ))}
             </datalist>
+          
+            
           </div>
-
+      <div className="absolute top-12"><Ghost /></div> {/* Positioned the Ghost at the bottom */}
           <Menu onChangePage={onChangePage} />
         </div>
       ) : null}
