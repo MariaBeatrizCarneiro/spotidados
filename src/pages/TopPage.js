eff9216e79e8b87cc4bd2a53d6179b73a2169d86
MariaBeatrizCarneiro <up202107798@up.pt>
2024-02-09 15:07:41 +0000

    function displayPeriodo(periodo)

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -15,6 +15,21 @@ export function TopPage({ onChangePage }) {
     }
   };
 
+  function displayPeriodo(periodo) {
+    switch (periodo) {
+      case "sempre":
+        return "desde sempre"
+      case "4semanas":
+        return "Últimas 4 semanas"
+      case "6meses":
+        return "Últimos 6 meses "
+      case "ultimoAno":
+        return "Último ano"
+      default:
+        return " "
+    }
+  }
+
 
   return (
     <div className="bg-lightgrey">
@@ -23,7 +38,7 @@ export function TopPage({ onChangePage }) {
 
       <div className="fixed w-full z-10 text-center bg-lightgrey">
         <p className="font-PressStart2p text-lg">Top #100</p>
-        <p className="pb-2 text-green font-PressStart2p text-xxs">{periodo}</p>
+        <p className="pb-2 text-green font-PressStart2p text-xxs">{displayPeriodo(periodo)}</p>
         <button onClick={() => { setExibirArtistas(false); }}
           className={`font-PressStart2p text-xs px-4 py-3 border-2 border-black bg-lightgrey ${!exibirArtistas ? "border-b-0 border-x-0" : ""}`} style={{ width: "50%" }}>
           Músicas
